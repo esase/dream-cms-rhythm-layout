179d14c87c74aba2c44b80c8ac4037d052e434e2
esase <alexermashev@gmail.com>
2015-09-11 17:47:10 +0600

    add release files

--- public_html/layout/rhytm/layout.config.install.php
+++ public_html/layout/rhytm/layout.config.install.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'compatable' => '2.2.3',
-    'version' => '1.0.0',
+    'compatable' => '2.3.0',
+    'version' => '1.0.2',
     'vendor' => 'eSASe',
     'vendor_email' => 'alexermashev@gmail.com'
 ];
